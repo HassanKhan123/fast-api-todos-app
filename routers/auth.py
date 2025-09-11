a9a875b42c9ad601c96a83e69bed8ac6569e678d
HassanKhan123 <hk260280@gmail.com>
2025-09-11 22:13:21 +0200

    added: create and login user

--- routers/auth.py
+++ routers/auth.py
@@ -1,8 +1,70 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from starlette import status
+from pydantic import BaseModel, Field
+from models import Users
+from passlib.context import CryptContext
+from database import SessionLocal
+from fastapi.security import OAuth2PasswordRequestForm
+
 
 router = APIRouter()
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@router.get("/auth")
-def get_user():
-    return {"message": "This is the auth endpoint"}
+
+class CreateUserRequest(BaseModel):
+    username: str = Field(min_length=3)
+    email: str
+    firstname: str = Field(min_length=3)
+    lastname: str = Field(min_length=3)
+    password: str = Field(min_length=6)
+    role: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    user_model = Users(
+        username=create_user_request.username,
+        email=create_user_request.email,
+        firstname=create_user_request.firstname,
+        lastname=create_user_request.lastname,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        role=create_user_request.role,
+        is_active=True
+    )
+
+    db.add(user_model)
+    db.commit()
+    return {"message": "User created successfully"}
+
+
+@router.post("/token", status_code=status.HTTP_200_OK)
+def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+
+    return {"message": "Login successful"}
