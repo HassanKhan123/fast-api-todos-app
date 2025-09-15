96e448ec41b0c27de90b0311e4d41793f8fc4e4d
HassanKhan123 <hk260280@gmail.com>
2025-09-15 16:19:56 +0200

    added: jwt creation

--- routers/auth.py
+++ routers/auth.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from datetime import timedelta, datetime, timezone
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from starlette import status
@@ -7,9 +8,13 @@ from models import Users
 from passlib.context import CryptContext
 from database import SessionLocal
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 
 router = APIRouter()
+
+SECRET_KEY = "b87f4f5a2f63518d5d67f5c1caf86d0647b68f7d4f3e4eb261ae24e059527b35"
+ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -21,6 +26,11 @@ class CreateUserRequest(BaseModel):
     lastname: str = Field(min_length=3)
     password: str = Field(min_length=6)
     role: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 def get_db():
@@ -43,6 +53,13 @@ def authenticate_user(db: Session, username: str, password: str):
     return user
 
 
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id}
+    expires = datetime.now(timezone.utc)+expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     user_model = Users(
@@ -60,11 +77,13 @@ def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     return {"message": "User created successfully"}
 
 
-@router.post("/token", status_code=status.HTTP_200_OK)
+@router.post("/token", response_model=Token, status_code=status.HTTP_200_OK)
 def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=400, detail="Incorrect username or password")
+    token = create_access_token(
+        username=user.username, user_id=user.id, expires_delta=timedelta(minutes=30))
 
-    return {"message": "Login successful"}
+    return {"access_token": token, "token_type": "bearer"}
